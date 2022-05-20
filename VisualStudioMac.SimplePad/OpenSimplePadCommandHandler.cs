acba507e6a856ba660cd956e0f9a2bc7e19d541b
Ivo Krugers <i.krugers@xablu.com>
2022-05-20 11:02:13 +0200

    Add project type and new menu

---
+++ VisualStudioMac.SimplePad/OpenSimplePadCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+
+namespace VisualStudioMac.SimplePad
+{
+    public class OpenSimplePadCommandHandler : CommandHandler
+    {
+        protected override void Run()
+        {
+            var pad = IdeApp.Workbench.GetPad<Gui.SimplePad>();
+            if (pad != null)
+            {
+                pad.Visible = true;
+                pad.IsOpenedAutomatically = true;
+                pad.BringToFront(true);
+            }
+        }
+
+        protected override void Update(CommandInfo info)
+        {
+            info.Enabled = true;
+        }
+    }
+}
